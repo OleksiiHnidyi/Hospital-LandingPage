d99d7cec7e82d93e8ca3bcb94870c967ae29875f
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-04-24 21:03:12 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
