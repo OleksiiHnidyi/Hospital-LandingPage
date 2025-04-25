580f3404d40f9cbc24ead27f5d087417c38bf702
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-04-25 19:29:12 +0300

    Update App.jsx and Footer.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 
-const App = () => {
+function App() {
   return (
     <div className="text-[#1d4d85] app min-w-[280px] min-h-screen bg-background">
       <Header />
