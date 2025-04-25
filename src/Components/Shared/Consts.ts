d9c9bdb1ecde80284b8cff157d440beb976130b6
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-04-25 19:31:28 +0300

    Add Consts.ts

---
+++ src/Components/Shared/Consts.ts
@@ -0,0 +1,16 @@
+import logo from '@/assets/Images/logo.png';
+
+export const FooterData = {
+  logo: logo,
+  addresses: ['4517 Washington Ave', 'Manchester, Kentucky 39495'],
+  phone: '+1 (505) 555-0125',
+  departments: [
+    'Cardiology',
+    'Dentistry',
+    'Neurology',
+    'Orthopedics',
+    'Surgery',
+    'More',
+  ],
+  links: links,
+};
