3cce7d92455012a42215f795e448bcad2485fc22
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-04-28 14:18:58 +0300

    Update ContactDiv

--- src/Components/Footer/ContactDiv.tsx
+++ src/Components/Footer/ContactDiv.tsx
@@ -4,7 +4,9 @@ import Input from '../UI/Input';
 const ContactDiv = () => {
   return (
     <div className="max-w-xs">
- 
+      <p className="ml-1 font-bold max-w-[250px]">
+        Have Something To Talk About With Our Professionals?
+      </p>
       <Input placeholder="Your Email" />
       <SocialsDiv />
     </div>
