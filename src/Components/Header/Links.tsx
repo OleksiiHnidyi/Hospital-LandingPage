56a2ac0340b902326ddd371e42ffd3535d065f78
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-05-10 16:54:45 +0300

    Update Header

--- src/Components/Header/Links.tsx
+++ src/Components/Header/Links.tsx
@@ -11,7 +11,12 @@ const Links = ({ selectedPage, setSelectedPage }: Props) => {
   return (
     <>
       {links.map((link) => (
-        
+        <Link
+          key={link}
+          page={link}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
       ))}
     </>
   );
