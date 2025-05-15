fbce8e962e0649ce89f065bcf2fe130c499e7f44
OleksiiHnidyi <oleksiihnidyi@gmail.com>
2025-05-15 03:59:54 +0300

    Update Doctor

--- src/Components/Main/Doctors/Doctors.tsx
+++ src/Components/Main/Doctors/Doctors.tsx
@@ -5,9 +5,6 @@ import Doctor from './Doctor';
 const Doctors = () => {
   return (
     <SectionWrapper id="doctors">
-      <h3 className="text-4xl font-bold text-center mb-20">
-        {DoctorsData.heading}
-      </h3>
       <div className="flex justify-between gap-10 overflow-auto px-5 md:p-0">
         {DoctorsData.doctors.map((doctor, index) => (
           <Doctor key={index} doc={doctor} />
